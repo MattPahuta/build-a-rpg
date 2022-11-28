4f6946d31f3b02a3c929a2dafcd3e86be6957658
Matt Pahuta <mhpahuta@gmail.com>
2022-11-28 15:30:22 -0800

    Add initial character objects, render function

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 
+// hero and monster objects
+const hero = {
+  elementId: 'hero',
+  name: 'Wizard',
+  avatar: 'images/wizard.png',
+  health: 60,
+  diceRoll: 6,
+}
 
-document.querySelector('#hero').innerHTML = `
-  <div class="character-card">
-    <h4 class="name"> Wizard </h4>
-    <img class="avatar" src="images/wizard.png"/>
-    <p class="health">health: <b> 60 </b></p>
-    <div class="dice-container"><div class="dice"> 6 </div></div>
-  </div>        
-  `
-document.querySelector('#monster').innerHTML = `
-  <div class="character-card">
-    <h4 class="name"> Orc </h4>
-    <img class="avatar" src="images/orc.png"/>
-    <p class="health">health: <b> 10 </b></p>
-    <div class="dice-container"><div class="dice"> 4 </div></div>
-  </div>
-  `
+const monster = {
+  elementId: 'monster',
+  name: 'Orc',
+  avatar: 'images/orc.png',
+  health: 10,
+  diceRoll: 4,
+}
+
+// render character
+function renderCharacter(data) {
+  // insert html into targeted div
+  document.getElementById(data.elementId).innerHTML = 
+   `<div class="character-card">
+      <h4 class="name">${data.name}</h4>
+      <img class="avatar" src="${data.avatar}"/>
+      <p class="health">health: <b>${data.health}</b></p>
+      <div class="dice-container"><div class="dice">${data.diceRoll}</div></div>
+    </div>`;
+}
+
+renderCharacter(hero)
+renderCharacter(monster)
