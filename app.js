b7a2014a82902c140651d928a790eb29b2797710
Matt Pahuta <mhpahuta@gmail.com>
2022-12-08 14:47:27 -0800

    Update attack func to handle next opponent logic

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 import characterData from './data.js'; // import from default export
 import Character from './Character.js'; // import from defualt export
 
+// monsters array
+let monstersArray = ['orc', 'demon', 'goblin'];
+
+function getNewMonster() {
+  const nextMonsterData = characterData[monstersArray.shift()];
+  // *** Note: nexMonsterData as 'question' in ternary:
+  // nextMonsterData evaluates to undefined (falsy) if there's no more monsters in array to assign as a value
+  return nextMonsterData ? new Character(nextMonsterData) : {}; 
+}
+
 // attack function
 function attack() {
   wizard.getDiceHtml()
-  orc.getDiceHtml()
-  wizard.takeDamage(orc.currentDiceScore)
-  orc.takeDamage(wizard.currentDiceScore)
+  monster.getDiceHtml()
+  wizard.takeDamage(monster.currentDiceScore)
+  monster.takeDamage(wizard.currentDiceScore)
   render(); // call render to get fresh dice rolls
-  if (wizard.dead || orc.dead) {
+
+  if (wizard.dead) { // if wizard is dead, end the game
     endGame()
+  } else if (monster.dead) {
+      if (monstersArray.length > 0) {
+        monster = getNewMonster()
+        render()
+      } else {
+        endGame()
+      }
   }
 }
 
 function endGame() {
-  const endMessage = wizard.health === 0 && orc.health === 0 ? 'No victors - all creatures dead'
+  const endMessage = wizard.health === 0 && monster.health === 0 ? 'No victors - all creatures dead'
     : wizard.health > 0 ? 'Wizard wins'
     : 'Orc wins'
 
@@ -31,13 +49,13 @@ function endGame() {
 // render charcaters v2.0
 function render() {
   document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
-  document.getElementById('monster').innerHTML = orc.getCharacterHtml()
+  document.getElementById('monster').innerHTML = monster.getCharacterHtml()
 }
 
 document.getElementById('attack-button').addEventListener('click', attack);
 
 // build characters
 const wizard = new Character(characterData.hero);
-const orc = new Character(characterData.monster);
-// call render
-render()
+let monster = getNewMonster();
+
+render() // render the game board and initial characterss
--- data.js
+++ data.js
@@ -8,13 +8,27 @@ const characterData = {
     diceCount: 3,
     currentDiceScore: [],
   },
-  monster: {
+  orc: {
     name: 'Orc',
     avatar: 'images/orc.png',
     health: 30,
     diceCount: 1,
     currentDiceScore: [],
-  }
+  },
+  demon: {
+    name: "Demon",
+    avatar: "images/demon.png",
+    health: 25,
+    diceCount: 2,
+    currentDiceScore: []
+  },
+  goblin: {
+    name: "Goblin",
+    avatar: "images/goblin.png",
+    health: 20,
+    diceCount: 3,
+    currentDiceScore: []
+  },
 }
 
 // default export
